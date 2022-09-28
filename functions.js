2b5d64709ad318afcd3aed6918a37ee7d4b707f1
DaniKef <gurenko888@gmail.com>
2022-09-28 22:15:21 +0300

    second commit

--- functions.js
+++ functions.js
@@ -1,10 +1,12 @@
 const navbarSupportedContent = document.getElementById('navbarSupportedContent');
 navbarSupportedContent.style.display ="none";
+const aboutUs = document.getElementById('aboutUs');
+aboutUs.style.display = "none";
 
 function OpenCLoseNavbar() {
     if(navbarSupportedContent.style.display =="none")
     {
-        document.getElementById('navbarSupportedContent').style.display = "block";
+        navbarSupportedContent.style.display = "block";
     }
     else 
     {
@@ -12,4 +14,16 @@ function OpenCLoseNavbar() {
     }
 }
 
+function OpenCLoseAboutUs() {
+    if(aboutUs.style.display =="none")
+    {
+        aboutUs.style.display = "block";
+    }
+    else 
+    {
+        aboutUs.style.display ="none";
+    }
+}
+
 document.getElementById('navbar-toggler-btn').addEventListener('click', OpenCLoseNavbar);
+document.getElementById('aboutUsBtn').addEventListener('click', OpenCLoseAboutUs);
